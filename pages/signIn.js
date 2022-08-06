9f5552f15530309130cc5ef423f97bdc33bae925
chineloobigwe <chinelo.obigwe@yahoo.com>
2022-08-06 14:40:49 +0100

    fixing up responsiveness and modal

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,49 +1,112 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import SignInModal from "../styles/signInModal.module.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 export default function signIn() {
-  return (
-    <>
-      <div className={` container-fluid ${SignInModal.signInModal}`}>
-        <p className="fw-bold fs-1 text-center pt-5">Sign In</p>
-        <button
-          href=""
-          className={`${SignInModal.signInModalGoogleButton}, ${SignInModal.otherElements}`}
-        >
-          Sign In using <b>Google</b>
-        </button>
+  // const [show, setShow] = useState(false);
+  // console.log(show);
+  return <></>;
+}
+{
+  /* <Modal>
+        {/* <Modal.Header closeButton> */
+}
+{
+  /* <Modal.Title>Modal</Modal.Title> */
+}
+{
+  /* </Modal.Header> */
+}
+{
+  /* <Modal.Body></Modal.Body> */
+}
+{
+  /* </Modal>  */
+}
+{
+  /* <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div> */
+}
 
-        <p className="text-center">
-          <Link href="">or sign in with your email</Link>
-        </p>
-        <div>
-          <p className={`mb-0 pb-0 ${SignInModal.labelsEmail}`}>
-            Email Address
+{
+  /* SIGN IN */
+}
+{
+  /* <div className="bg-light">
+        <div className={` container-fluid ${SignInModal.signInModal}`}>
+          <p className="fw-bold fs-1 text-center pt-5">Sign In</p>
+          <button
+            href=""
+            className={`${SignInModal.signInModalGoogleButton}, ${SignInModal.otherElements}`}
+          >
+            Sign In using <b>Google</b>
+          </button>
+
+          <p className="text-center">
+            <Link href="">or sign in with your email</Link>
           </p>
-          <input
-            type="Email Address"
-            placeholder="Email Address"
-            className={`${SignInModal.inputFields} `}
-          />
+          <div>
+            <p className={`mb-0 pb-0 ${SignInModal.labelsEmail}`}>
+              Email Address
+            </p>
+            <input
+              type="Email Address"
+              placeholder="Email Address"
+              className={`${SignInModal.inputFields} `}
+            />
+          </div>
+          <div>
+            <p className={`mb-0 pb-0 ${SignInModal.labels}`}>Password</p>
+            <input
+              type="password"
+              placeholder="Password"
+              className={`${SignInModal.inputFields} `}
+            />
+          </div>
+          <button
+            className={`${SignInModal.signInModalButton} ${SignInModal.otherElements}`}
+          >
+            Sign In
+          </button>
+          <p className="text-center justify-content-center">
+            <Link href="/signUp">New to Uniswitch? Sign up here</Link>
+          </p>
         </div>
-        <div>
-          <p className={`mb-0 pb-0 ${SignInModal.labels}`}>Password</p>
-          <input
-            type="password"
-            placeholder="Password"
-            className={`${SignInModal.inputFields} `}
-          />
-        </div>
-        <button
-          className={`${SignInModal.signInModalButton} ${SignInModal.otherElements}`}
-        >
-          Sign In
-        </button>
-        <p className="text-center justify-content-center">
-          <Link href="/signUp">New to Uniswitch? Sign up here</Link>
-        </p>
-      </div>
-    </>
-  );
+      </div> */
 }
