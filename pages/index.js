4e8c0a4bab5569a66c131f402afa33240c34f9f4
Chigozie <ccasadu15@gmail.com>
2022-08-08 09:39:59 +0100

    Overall changes

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,13 @@ import Navbar from "./LandingPage/navbar";
 import Header from "./LandingPage/homeHeader";
 import Footer from "./LandingPage/footer";
 import HomePage from "../styles/homePage.module.css";
-// import SignInModal from "../styles/signInModal.module.css";
 
 export default function home() {
   return (
     <>
-
       <div className={` container-fluid ${HomePage.body}`}>
         <div className={``}>
-    <Navbar />
+          <Navbar />
           <Header />
         </div>
       </div>
